2992dd3c017446bed40256d4b31810dddd0ac570
Jerome Franco <jerfranco@deloitte.com>
2019-06-23 11:21:14 +0800

    Configured eslint

--- app/components/hello-world/hello-world.style.ts
+++ app/components/hello-world/hello-world.style.ts
@@ -2,15 +2,15 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    flex: 0,
+    justifyContent: 'center',
   },
   text: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
+    textAlign: 'center',
   },
 });
 
